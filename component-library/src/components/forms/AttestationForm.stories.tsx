bbba0c1b0b50a63a3210c3d4ae56fa4ab4c9b933
Peter Kompasz <kompaszbence@gmail.com>
2024-08-10 23:28:43 +0200

    chore: form files

---
+++ component-library/src/components/forms/AttestationForm.stories.tsx
@@ -0,0 +1,37 @@
+import { StoryFn, Meta } from "@storybook/react";
+import AttestationForm from "./AttestationForm";
+import { JSX } from "react/jsx-runtime";
+import { FormProps } from "./AttestationForm.types";
+import { ethers } from "ethers";
+
+export default {
+  title: "ReactComponentLibrary/Forms",
+  component: AttestationForm,
+} as Meta<typeof AttestationForm>;
+
+const Template: StoryFn<typeof AttestationForm> = (
+  args: JSX.IntrinsicAttributes & FormProps
+) => <AttestationForm {...args} />;
+
+const alchemyApiUrl = `https://eth-sepolia.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}`;
+const network = "sepolia";
+const provider = new ethers.JsonRpcProvider(alchemyApiUrl, network);
+
+const signer = ethers.Wallet.createRandom().connect(provider);
+
+console.log("Address:", signer.address);
+console.log("Private Key:", signer.privateKey);
+
+
+export const FormSimpleTest = Template.bind({});
+FormSimpleTest.args = {
+  schemaUid:
+    "0x6dee028cb86e60e2884fe261bd0c4e701f7cdfaea0e42aec5628ec96d4b3e10f",
+  title: "Simple attestation",
+  description: "hey checkout my attestation",
+  theme: "primary",
+  testIdPrefix: "badge",
+  provider,
+  signer 
+};
+
---
+++ component-library/src/components/forms/AttestationForm.types.ts
@@ -0,0 +1,21 @@
+import { Signer, Provider } from "ethers";
+
+type Group = {
+  className: string;
+  fields: string[];
+};
+
+export interface FormProps {
+  testIdPrefix: string;
+  schemaUid: string;
+  provider: Provider;
+  signer: Signer;
+  theme: "primary" | "secondary";
+  title?: string;
+  description?: string;
+  buttonText?: string;
+  groups?: Group[];
+  recipient: string;
+  expirationDate: number;
+  revocable: boolean;
+}
