3cc517817b8237568531617c04f5ab8701e5f20d
Peter Kompasz <kompaszbence@gmail.com>
2024-08-10 15:02:21 +0200

    chore: restructure

--- component-library/src/components/proof/AttestationBadge.types.ts
+++
@@ -1,8 +0,0 @@
-export interface DisplayProps {
-  testIdPrefix: string;
-  attestationAddress?: string;
-  theme: "primary" | "secondary";
-  title?: string;
-  description?: string;
-  disabled?: boolean;
-}
